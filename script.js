c3aebafbbaebc690142ddb2cc9eb3c346ea55bb3
amirraza24 <105011146+amirraza24@users.noreply.github.com>
2022-09-25 16:49:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const tipcalcy = () => {
-    let amount = document.getElementById('bill_amt').value;
-    let perct = document.getElementById('tip_percnt').value;
-    let tip =   amount*(perct/100);
-    let total = tip+ Number(amount);
-    document.getElementById('tip_amt').value =tip;
-    document.getElementById('total_bill').value = total;
-}
